29fe1d8c1b95d4c8d848b3b1e453719d8a226975
tranbatong <tongxayda00@gmail.com>
2026-04-04 23:43:48 +0700

    feat: add post

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { Routes, Route } from 'react-router-dom'
 import LandingPage from './pages/landing'
 import LoginPage from './pages/login/LoginPage'
 import RegisterPage from './pages/register/RegisterPage'
+import ViewPostPage from './pages/posts/ViewPostPage'
+import CreatePostPage from './pages/posts/CreatePostPage'
+import EditPostPage from './pages/posts/EditPostPage'
 
 function App() {
   return (
@@ -9,6 +12,9 @@ function App() {
       <Route path="/" element={<LandingPage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
+      <Route path="/posts/create" element={<CreatePostPage />} />
+      <Route path="/posts/:postId/edit" element={<EditPostPage />} />
+      <Route path="/posts/:postId?" element={<ViewPostPage />} />
     </Routes>
   )
 }
